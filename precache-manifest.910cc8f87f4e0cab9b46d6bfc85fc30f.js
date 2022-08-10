2982f9607e8f20cc1f5c14cfba7a264f94a6c3d0
Arjan Frans <arjan.frans@fusonic.net>
2022-08-10 15:36:50 +0200

    Updates

---
+++ precache-manifest.910cc8f87f4e0cab9b46d6bfc85fc30f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a67dc77d73424223a7428629a4e9dc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d20e5205e17b1fd9f51a",
+    "url": "/static/css/main.d74cf6b8.chunk.css"
+  },
+  {
+    "revision": "d3aec1e4fe64f3247d8c",
+    "url": "/static/js/2.3638b311.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3638b311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d20e5205e17b1fd9f51a",
+    "url": "/static/js/main.5fb3f845.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.5fb3f845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d15ae5080dd206a1ae",
+    "url": "/static/js/runtime-main.9d50c4af.js"
+  }
+]);
